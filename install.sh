439b7da5140e2d75dcdd58755f01328af6f4fbaf
jittervpn <diegogz143@gmail.com>
2026-06-10 13:43:52 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,7 +17,12 @@ curl -fsSL "$REPO_RAW/menu.sh" -o /usr/local/bin/menu || {
   echo "❌ Error al descargar menu.sh"
   exit 1
 }
-
+cat >> /etc/ssh/sshd_config << 'EOF'
+KexAlgorithms +diffie-hellman-group14-sha1,diffie-hellman-group1-sha1
+Ciphers +aes128-cbc,3des-cbc,aes256-cbc
+MACs +hmac-sha1,hmac-md5
+EOF
+systemctl restart sshd
 sed -i 's/\r$//' /usr/local/bin/menu
 chmod +x /usr/local/bin/menu
 
